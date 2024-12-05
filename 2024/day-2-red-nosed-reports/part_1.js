f6148c2bf1dc963a53bab3795a846b3647ae831b
travis_witt <travis_witt@sweetwater.com>
2024-12-05 16:38:19 -0500

    started day 2 part 1 (again)

---
+++ 2024/day-2-red-nosed-reports/part_1.js
@@ -0,0 +1,27 @@
+// Advent of Code 2024 Day 2: Red-Nosed Reports (Part 1)
+import puzzle_data from './input'
+
+let input = puzzle_data.puzzle_data;
+
+function isOrdered(report) {
+  const asc = report.every((x, i, rep) => i === 0 || x < rep[i - 1]);
+  const desc = report.every((x, i, rep) => i === 0 || x > rep[i - 1]);
+
+  return asc || desc;
+}
+
+function inRange(report) {
+  return report.every((x, i, rep) => {
+    let diff = Math.abs(x - rep[i]);
+    return diff >= 1 && diff <= 3;
+  })
+}
+
+let safe = 0;
+input.forEach((report) => {
+  if (isOrdered(report) && inRange(report)) {
+    safe++;
+  };
+});
+
+console.log(safe);
